5b5204937860540fc83cb330f5fa33cfaed23bf6
Karim <Karim13.sab@gmail.com>
2019-04-09 19:55:00 +0300

    add a test

---
+++ app/src/test/java/com/karimsabitov/headmanlog/ReportCreatorTest.java
@@ -0,0 +1,16 @@
+package com.karimsabitov.headmanlog;
+
+import org.junit.Test;
+
+/**
+ * Created by User on 09.04.2019.
+ */
+
+public class ReportCreatorTest {
+
+    @Test
+    public void test(){
+
+    }
+
+}
